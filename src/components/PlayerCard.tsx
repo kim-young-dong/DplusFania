1351c4a75add2ce4b74575cc432beb0f96a7ef29
kim-young-dong <opa9904@gmail.com>
2024-06-21 17:09:22 +0900

    style: 스타일드 컴포넌트 수정 - inline style로 회전각 변경: class 해쉬값이 너무 많이 생기는 것을 방지 - interating, isActive state 추가: active 상태로의 전환 도중 인터렉션을 막기 위함

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -22,50 +22,90 @@ const clamp = (value: number, min = 0, max = 100) => {
 
 const PlayerCard = ({ card }: { card: Card }) => {
   const cardRef = useRef<HTMLDivElement>(null);
-  const [coordinate, setCoordinate] = useState({ x: 0, y: 0 });
-  // 카드 회전 각도
-  const cardTransform = useMemo(() => {
-    if (coordinate.x === 0 && coordinate.y === 0) return { sec: 1, x: 0, y: 0 };
-    return {
-      sec: 0,
-      x: (coordinate.y / 490) * 80 - 40,
-      y: (coordinate.x / 340) * 80 - 40,
-    };
-  }, [coordinate]);
+  const [isActive, setIsActive] = useState(false);
+  const [interacting, setInteracting] = useState(true);
+  const [pointer, setPointer] = useState({ x: 0, y: 0 });
+  const [transform, setTransform] = useState({
+    sec: 0,
+    rotateX: 0,
+    rotateY: 0,
+  });
+  // 카드가 active 상태일 때 360도 회전된 상태로 변경
+  const isTurn = useMemo(() => {
+    return isActive ? 360 : 0;
+  }, [isActive]);
 
   // 광택 효과
   const overlayStyle = useMemo(() => {
-    const percent = {
-      x: clamp(round((100 / 340) * coordinate.x)),
-      y: clamp(round((100 / 490) * coordinate.y)),
-    };
+    // interacting 상태가 아닐 때 리턴
+    if (interacting === false) return { x: 0, y: 0 };
 
+    const percent = {
+      x: clamp(round((100 / 340) * pointer.x)),
+      y: clamp(round((100 / 490) * pointer.y)),
+    };
     return {
       x: `${percent.x}%`,
       y: `${percent.y}%`,
     };
-  }, [coordinate, cardRef.current]);
+  }, [pointer, interacting]);
 
   // 마우스 이동 이벤트
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    setCoordinate({
-      x: event.nativeEvent.offsetX,
-      y: event.nativeEvent.offsetY,
+    // interacting 상태가 아닐 때 리턴
+    if (interacting === false) return;
+
+    const offsetX = event.nativeEvent.offsetX;
+    const offsetY = event.nativeEvent.offsetY;
+
+    // 카드 회전 스타일링
+    setTransform({
+      sec: 0,
+      rotateX: (offsetY / 490) * 80 - 40,
+      rotateY: (offsetX / 340) * 80 - 40 + isTurn,
+    });
+
+    // 마우스 포인터 위치 변경
+    setPointer({
+      x: offsetX,
+      y: offsetY,
     });
   };
+  // 클릭 이벤트
+  const activate = () => {
+    // active 상태 변경
+    setIsActive((pre) => !pre);
+    // pointer 초기화, 카드 회전
+    isActive
+      ? setTransform({ sec: 1, rotateX: 0, rotateY: 0 })
+      : setTransform({ sec: 1, rotateX: 0, rotateY: 360 });
+    setPointer({ x: 0, y: 0 });
+    // interacting 상태 변경, 1초 후 재활성화
+    setInteracting(false);
+    setTimeout(() => {
+      setInteracting(true);
+    }, 1000);
+  };
   return (
-    <div
-      style={{
-        perspective: "500px",
-      }}
-    >
+    <CardTransLater>
       <Card
-        sec={cardTransform.sec}
-        rotateX={cardTransform.x}
-        rotateY={cardTransform.y}
-        className="player_card"
+        style={{
+          transform: `rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg)`,
+          transition: `transform ${transform.sec}s`,
+        }}
         onMouseMove={handleMouseMove}
-        onMouseOut={() => setCoordinate({ x: 0, y: 0 })}
+        onMouseOut={() => {
+          // interacting 상태일 때만 초기화
+          if (interacting === true) {
+            setPointer({ x: 0, y: 0 });
+            setTransform({
+              sec: 1,
+              rotateX: 0,
+              rotateY: 0 + isTurn,
+            });
+          }
+        }}
+        onClick={activate}
         ref={cardRef}
       >
         <div
@@ -79,9 +119,17 @@ const PlayerCard = ({ card }: { card: Card }) => {
               hsla(0, 0%, 0%, 0) 90%
             )            
             `,
-            opacity: coordinate.x === 0 && coordinate.y === 0 ? 0 : 0.8,
+            opacity: pointer.x === 0 && pointer.y === 0 ? 0 : 0.8,
           }}
         ></div>
+        <div className="card_back">
+          <Image
+            src={"/images/cards/card_back.png"}
+            alt={"card_back"}
+            width={340}
+            height={490}
+          />
+        </div>
         <div className="card_front">
           <Image
             src={card.img}
@@ -90,26 +138,33 @@ const PlayerCard = ({ card }: { card: Card }) => {
             height={490}
           />
         </div>
-        {/* <div className="card_back">
-          <Image
-            src={"/images/cards/card_back.png"}
-            alt={"card_back"}
-            width={340}
-            height={490}
-          />
-        </div> */}
       </Card>
-    </div>
+    </CardTransLater>
   );
 };
 
 export default PlayerCard;
-
-const Card = styled.div<{ sec: number; rotateX: number; rotateY: number }>`
+const CardTransLater = styled.div`
+  perspective: 600px;
   position: relative;
-  transition: transform ${(props) => props.sec}s;
-  transform: ${(props) =>
-    `rotateX(${props.rotateX}deg) rotateY(${props.rotateY}deg)`};
+  width: auto;
+  border-radius: 60px;
+`;
+const Card = styled.div`
+  display: grid;
+  will-change: transform, box-shadow;
+  box-shadow: 0 0 3px -1px transparent, 0 0 2px 1px transparent,
+    0 0 5px 0px transparent, 0px 10px 20px -5px black, 0 2px 15px -5px black,
+    0 0 20px 0px transparent;
+  border-radius: 15px;
+  overflow: hidden;
+
+  * {
+    width: auto;
+    height: auto;
+    display: grid;
+    grid-area: 1/1;
+  }
 
   .card__glare {
     width: 100%;
