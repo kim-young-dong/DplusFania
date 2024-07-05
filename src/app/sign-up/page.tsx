e9b85ea32b56eab9d0a3cf14a3b0631ba4f13a55
kim-young-dong <opa9904@gmail.com>
2024-07-05 21:44:08 +0900

    feat: signup spinner 추가

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -11,6 +11,7 @@ interface ChildComponentProps {
 }
 
 const SignupForm: React.FC<ChildComponentProps> = ({ onIsCompleteChange }) => {
+  const [isLoad, setIsLoad] = useState<boolean>(false);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [passwordCheck, setPasswordCheck] = useState<string>("");
@@ -43,6 +44,7 @@ const SignupForm: React.FC<ChildComponentProps> = ({ onIsCompleteChange }) => {
   };
 
   const handleSignup = async (e: FormEvent) => {
+    setIsLoad(true);
     e.preventDefault();
 
     if (!validatePassword()) {
@@ -64,48 +66,76 @@ const SignupForm: React.FC<ChildComponentProps> = ({ onIsCompleteChange }) => {
         setError(data.message);
       }
     }
+    setIsLoad(false);
   };
   return (
-    <form onSubmit={handleSignup} className="w-full">
-      <Input
-        id="email"
-        title="이메일 주소"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        value={email}
-        type="email"
-        errorMessage="text"
-      />
-      <Input
-        id="password"
-        title="비밀번호"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-        type="password"
-        errorMessage="text"
-      />
-      <Input
-        id="passwordCheck"
-        title="비밀번호 확인"
-        onChange={(e) => setPasswordCheck(e.target.value)}
-        value={passwordCheck}
-        type="password"
-        errorMessage="text"
-      />
-      <div style={{ margin: "14px 0" }}>
-        <Button type="submit">회원가입</Button>
-      </div>
-      <div
-        style={{
-          color: "red",
-          fontSize: "14px",
-          textAlign: "center",
-        }}
-      >
-        {error}
-      </div>
-    </form>
+    <>
+      {isLoad ? (
+        <>
+          <svg
+            className="animate-spin h-12 w-12 text-blue-400"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </>
+      ) : (
+        <form onSubmit={handleSignup} className="w-full">
+          <Input
+            id="email"
+            title="이메일 주소"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+            type="email"
+            errorMessage="text"
+          />
+          <Input
+            id="password"
+            title="비밀번호"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            errorMessage="text"
+          />
+          <Input
+            id="passwordCheck"
+            title="비밀번호 확인"
+            onChange={(e) => setPasswordCheck(e.target.value)}
+            value={passwordCheck}
+            type="password"
+            errorMessage="text"
+          />
+          <div style={{ margin: "14px 0" }}>
+            <Button type="submit">회원가입</Button>
+          </div>
+          <div
+            style={{
+              color: "red",
+              fontSize: "14px",
+              textAlign: "center",
+            }}
+          >
+            {error}
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 const SignupConfirm = () => {
