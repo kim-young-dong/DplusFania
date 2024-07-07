71b9a9403e87cab6bee4454d68b660e97dd8f6c8
kim-young-dong <opa9904@gmail.com>
2024-07-07 11:59:28 +0900

    feat: zod 추가, 미들웨어 수정 미들웨어의 역할을 유저 권한에 따른 페이지 리디렉션으로 정의

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,27 +1,18 @@
+"use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import PlayerCard from "@/components/PlayerCard";
 import Button from "@/components/Button";
 import { GET_RANDOM_CARD } from "@/constant/card";
 
-export default function Home() {
+export default function HomePage() {
+  useEffect(() => {}, []);
   const cardInfo = GET_RANDOM_CARD();
 
   return (
     <>
       <div className="w-full flex flex-col items-center gap-4 text-center">
-        <h1 className="text-3xl font-bold">
-          Welcome to
-          <br /> Dplus Fania
-        </h1>
-        <div className="w-full flex flex-col justify-center items-center gap-8">
-          <p className="text-xl">
-            자신이 뽑은 카드를
-            <br /> 컬렉션에 추가해보세요!
-          </p>
-          <Link href={"/sign-in"} className="w-full">
-            <Button>로그인</Button>
-          </Link>
-        </div>
+        <h1 className="text-3xl font-bold">오늘의 카드</h1>
       </div>
       <div className="flex flex-col items-center gap-2">
         <p>Click to Card</p>
