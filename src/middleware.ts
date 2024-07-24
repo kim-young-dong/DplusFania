0b759869a3438599970fd2392ec3d75f4040a4e7
kim-young-dong <opa9904@gmail.com>
2024-07-24 14:03:33 +0900

    feat: zod 추가, 미들웨어 수정 미들웨어의 역할을 유저 권한에 따른 페이지 리디렉션으로 정의

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,22 @@
 // lib/auth.ts
-import { NextApiRequest, NextApiResponse } from "next";
 import { $supabase } from "@/app/lib/supabase";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-export async function middleware(req: NextApiRequest) {
-  const token = req.cookies.access_token;
+export async function middleware(req: NextRequest) {
+  const access_token = req.cookies.get("access_token")?.value;
+  const refresh_token = req.cookies.get("refresh_token")?.value;
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
+  if (access_token && refresh_token) {
+    switch (true) {
+      case req.nextUrl.pathname == "/sign-in":
+      case req.nextUrl.pathname == "/sign-up":
+      case req.nextUrl.pathname == "/find-id":
+      case req.nextUrl.pathname == "/find-password":
+        return NextResponse.redirect(new URL("/", req.url));
+    }
   }
-
-  const {
-    data: { user },
-    error,
-  } = await $supabase.auth.getUser(token);
-
-  if (error) {
-    return null;
-  }
-
-  return user;
 }
 export const config = {
-  matcher: ["/home/:path*"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
